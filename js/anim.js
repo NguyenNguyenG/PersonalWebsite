95099a6af9e161c41d841fd5718edc47998e04eb
nguyen nguyen <nguyennguyen98@g.ucla.edu>
2020-07-17 20:20:01 -0700

    Added scrolling bar

--- js/anim.js
+++ js/anim.js
@@ -15,3 +15,29 @@ $(document).ready(function () {
 	});
 
 });
+
+$(document).on('scroll', function() {
+	console.log(($('#skills').position().top - $(this).scrollTop()));
+	console.log(($('#skills').position().top - $(this).scrollTop()) <= 560)
+    if(($('#skills').position().top - $(this).scrollTop()) <= 560){
+		$('#skill-tag').addClass('active');
+		$('#abt-tag').removeClass('active');
+		$('#exp-tag').removeClass('active');
+		$('#edu-tag').removeClass('active');
+    } else if ($(this).scrollTop()>=$('#education').position().top){
+		$('#edu-tag').addClass('active');
+		$('#abt-tag').removeClass('active');
+		$('#exp-tag').removeClass('active');
+		$('#skill-tag').removeClass('active');
+    } else if ($(this).scrollTop()>=$('#experience').position().top){
+		$('#exp-tag').addClass('active');
+		$('#edu-tag').removeClass('active');
+		$('#abt-tag').removeClass('active');
+		$('#skill-tag').removeClass('active');
+    } else {
+		$('#abt-tag').addClass('active');
+		$('#edu-tag').removeClass('active');
+		$('#exp-tag').removeClass('active');
+		$('#skill-tag').removeClass('active');
+    }
+})
